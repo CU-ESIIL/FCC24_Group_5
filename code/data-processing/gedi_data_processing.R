b5db5e060433faf3a55eb157f0535ccbab65a415
king0708 <rking@nceas.ucsb.edu>
2024-03-13 23:46:41 +0000

    adds biomass for burned region

--- code/data-processing/gedi_data_processing.R
+++ code/data-processing/gedi_data_processing.R
@@ -79,3 +79,88 @@ sum(values(gedi_ab_soro2, na.rm = TRUE))/(1000000*2)  # megagrams to million met
 # 345787216
 
 plot(gedi_rast_elev_aea)
+
+# make a map of the high elevation region
+high_elev_bin <- ifel(is.na(elev_reclass), 0, 1)
+high_elev_sf <- st_as_sf(
+  as.polygons(high_elev_bin)
+)
+
+high_elev_sf <- high_elev_sf %>% 
+  filter(fileab72750c79d == 1)
+
+ 
+
+# Make Map
+
+ggplot() +
+  tidyterra::geom_spatraster(data = gedi_rast_aea) +
+  geom_sf(data = high_elev_sf, fill = "grey", color = "white") +
+  scale_fill_viridis_c(
+    name = "Biomass (Mg ha<sup>-1</sup>)",
+    limits = c(0, 250), na.value = "transparent") +
+  theme_void() + 
+  theme(legend.title = element_markdown()) +
+  ggspatial::annotation_north_arrow(
+    pad_y = unit(1, "cm"),
+    which_north = "true"
+  ) + 
+  ggspatial::annotation_scale() 
+
+
+# Bring in the wildfire data
+
+fire_events_sf <- read_sf("southern_rockies_fire_events_stats.gpkg")
+no_events_sf <- read_sf("southern_rockies_eventless.geojson")
+
+colnames(fire_events_sf)
+
+# filter events to fire between 2002 - 2005
+# create categories for forest 
+fire_events_sf <- fire_events_sf %>% 
+  filter(between(Year, 2002, 2005)) 
+
+fire_events_6933 <- st_transform(fire_events_sf, "epsg:6933")
+high_elev_6933 <- st_transform(high_elev_sf, crs = "epsg:6933")
+
+
+ggplot() +
+  geom_sf(data = high_elev_6933, fill = "white") +
+  geom_sf(data = fire_events_6933, fill = "red")
+
+fire_events_high_elev <- fire_events_6933 %>% 
+  st_filter(high_elev_6933, .pred = st_intersects)
+
+no_events_high_elev <- no_events_sf %>% 
+  st_transform(crs = "epsg:6933") %>%
+  st_filter(high_elev_6933, .pred = st_intersects)
+
+fire_events_high_elev_m <- fire_events_high_elev %>% 
+  group_by(Event_Type) %>%
+  st_union()
+
+# create vectors
+fire_events_high_elev_aea <- st_transform(fire_events_high_elev_m, crs = st_crs(gedi_ab_soro))
+no_events_aea <- st_transform(no_events_high_elev, crs = st_crs(gedi_ab_soro))
+
+fire_events_vec <- vect(fire_events_high_elev_aea)
+no_events_vec <- vect(no_events_aea)
+
+no_events_rast <- terra::rasterize(no_events_vec, gedi_ab_soro)
+
+terra::zonal(x = gedi_ab_soro, fire_events_vec, fun = "sum", na.rm = TRUE)
+terra::zonal(x = gedi_ab_soro, no_events_vec, fun = "sum", na.rm = TRUE)
+
+# 52651.24 Mg 
+# 5.265 million metric tons of C in fire areas
+
+# 
+
+
+
+
+ggplot() +
+  tidyterra::geom_spatraster(data = gedi_rast_mask) + 
+  geom_sf(data = fire_events_6933, fill ="red") 
+
+
